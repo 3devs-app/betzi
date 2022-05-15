31b201b249859c6a4ff2bdb8d9dfcb5214796bde
DanielF <daniel.yopla@cleevio.com>
2022-05-15 21:43:14 +0200

    Initial commit

---
+++ Betzi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Betzi
+//
+//  Created by Daniel Fernandez Yopla on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
