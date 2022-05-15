58a33cb0b805000543101f919a9f8cdade496b8b
DanielF <daniel.yopla@cleevio.com>
2022-05-15 22:10:18 +0200

    Structuring folders

---
+++ Betzi/Services/BetService.swift
@@ -0,0 +1,18 @@
+//
+//  BetService.swift
+//  Betzi
+//
+//  Created by Daniel Fernandez Yopla on 15.05.2022.
+//
+
+import Foundation
+
+protocol BetServiceType {
+    func createBet()
+}
+
+class BetService: BetServiceType {
+    func createBet() {
+        // firebase
+    }
+}
