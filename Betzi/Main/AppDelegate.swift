dc35f4e266980facbbc81afa215e02a7ef1d4d34
Luis Chavez <lchavezpozo@gmail.com>
2022-06-20 12:02:29 -0500

    remove packages

--- Betzi/Main/AppDelegate.swift
+++ Betzi/Main/AppDelegate.swift
@@ -6,14 +6,13 @@
 //
 
 import UIKit
-import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+       
         print("API KEY \(String(describing: Environment.get(type: String.self, for: .ApiKey)))")
         print("API KEY \(String(describing: Environment.get(type: URL.self, for: .baseURL)))")
         return true
