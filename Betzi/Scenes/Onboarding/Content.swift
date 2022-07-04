b9f19d945bed1d57c025b8e7d81e3d95c92090bc
DanielF <daniel.yopla@cleevio.com>
2022-07-04 17:45:16 +0200

    Adding R.swift and onboarding setup

---
+++ Betzi/Scenes/Onboarding/Content.swift
@@ -0,0 +1,72 @@
+//
+//  Content.swift
+//  Betzi
+//
+//  Created by Daniel Fernandez Yopla on 04.07.2022.
+//
+
+import Foundation
+import Rswift
+
+extension OnboardingView {
+    enum Content: Int, Identifiable, CaseIterable {
+        case page1 = 0
+        case page2
+        case page3
+
+        var id: Int { rawValue }
+
+        var title: String {
+            switch self {
+            case .page1:
+                return "Start \nbetting \nwith Betzi"
+            case .page2:
+                return "World \ncup"
+            case .page3:
+                return "Customize \nyour bets"
+            }
+        }
+
+        var subtitle: String {
+            switch self {
+            case .page1:
+                return "and have fun!"
+            case .page2:
+                return "and more coming!"
+            case .page3:
+                return "and invite your friends to play"
+            }
+        }
+
+        var description: String {
+            switch self {
+            case .page1:
+                return "Classic, custom bets and mini bets between friends"
+            case .page2:
+                return "Win betting during your favorite leagues"
+            case .page3:
+                return "You can also create mini bets between matches"
+            }
+        }
+        
+        var backgroundColor: ColorResource {
+            switch self {
+            case .page1:
+                return Colors.accents.yellowAmber
+            case .page2:
+                return Colors.accents.greenSoccerField
+            case .page3:
+                return Colors.accents.cyanBaby
+            }
+        }
+
+        var foregroundColor: ColorResource {
+            switch self {
+            case .page1:
+                return Colors.primary.midnightBlue
+            case .page2, .page3:
+                return Colors.primary.white
+            }
+        }
+    }
+}
--- Betzi/Scenes/Onboarding/OnboardingView.swift
+++ Betzi/Scenes/Onboarding/OnboardingView.swift
@@ -9,37 +9,115 @@ import SwiftUI
 
 struct OnboardingView: View {
     @ObservedObject var viewModel: OnboardingViewModel
+    @State private var pageIndex = 0
+    @State private var textsSize: CGSize = .zero
+
+    var currentContent: Content {
+        Content(rawValue: pageIndex) ?? .page1
+    }
+
+    var soccerBallYOffset: CGFloat {
+        switch currentContent {
+        case .page1:
+            return -500
+        case .page2:
+            return 20
+        case .page3:
+            return 200
+        }
+    }
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Start \nbetting \nwith Betzi")
-                .textStyle(.largeTitle)
-            Text("and have fun!")
-                .textStyle(.body)
-            Text("Classic, custom bets and mini bets between friends")
-                .textStyle(.bodyBold)
-            
-            Text(Environment.get(type: String.self, for: .ApiKey) ?? "EMPTY")
-                .textStyle(.bodyBold)
-            
-            Button(action: { print("Sign in with Apple") }, label: {
-                HStack {
-                    Image(systemName: "applelogo")
-                    Text("Sign in with Apple")
-                        .textStyle(.buttonLabelLarge)
-                }
-                .frame(maxWidth: .infinity)
-            })
-            .padding()
-            .foregroundColor(.white)
-            .background(
-                RoundedRectangle(cornerRadius: 10)
-                    .fill(.black)
-            )
-            .padding()
+        ZStack {
+            tabs
+
+            header
+
+            VStack(spacing: .xLarge) {
+                signInWithAppleButton
+
+                tabDots
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
         }
-        .frame(maxWidth: .infinity)
-        .padding(.medium)
+        .padding(.large)
+        .animation(.easeInOut, value: pageIndex)
+        .backgroundIgnoringSafeArea(currentContent.backgroundColor, edges: .all)
+        .navigationBarHidden(true)
+    }
+
+    private var header: some View {
+        HStack {
+            Text(String(currentContent.id + 1))
+                .textStyle(.largeTitle)
+                .foregroundColor(currentContent.foregroundColor)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Images.onboarding.soccerBall.image
+                .offset(y: soccerBallYOffset)
+                .animation(.spring(response: 1, dampingFraction: 0.6, blendDuration: 0.5), value: pageIndex)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+
+    private var tabs: some View {
+        TabView(selection: $pageIndex) {
+            ForEach(Content.allCases) { content in
+                makePage(with: content)
+                    .tag(content.id)
+            }
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+    }
+
+    private var tabDots: some View {
+        HStack(spacing: .xSmall) {
+            ForEach(Content.allCases) { content in
+                Circle()
+                    .fill(Colors.primary.white)
+                    .opacity(content.id == pageIndex ? 1 : 0.5)
+                    .frame(width: 8, height: 8)
+            }
+        }
+    }
+
+    private var signInWithAppleButton: some View {
+        Button(action: { print("Sign in with Apple") }, label: {
+            HStack {
+                Image(systemName: "applelogo")
+                Text("Sign in with Apple")
+                    .textStyle(.buttonLabelLarge)
+            }
+            .frame(maxWidth: .infinity)
+        })
+        .padding()
+        .foregroundColor(Colors.primary.white)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Colors.primary.midnightBlue)
+        )
+        .padding(.horizontal, .medium)
+    }
+
+    @ViewBuilder private func makePage(with content: Content) -> some View {
+        VStack(alignment: .leading, spacing: .medium) {
+            Text(content.title)
+                .textStyle(.largeTitle)
+
+            Text(content.subtitle)
+                .textStyle(.body)
+                .padding(.bottom, .large)
+
+            Text(content.description)
+                .textStyle(.bodyBold)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .readSize(onChange: { size in
+            if size.height > textsSize.height {
+                textsSize = size
+            }
+        })
+        .foregroundColor(currentContent.foregroundColor)
     }
 }
 
