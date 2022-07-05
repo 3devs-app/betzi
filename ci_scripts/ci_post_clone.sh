80fc43ea15096f57f4d428200878a08ccb5474f0
DanielF <daniel.yopla@cleevio.com>
2022-07-05 13:30:22 +0200

    Updating post_clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 brew install mint
+cd ../
 mint bootstrap
 
-cat > ../Betzi/Configs/Environment/Confg-${CONFIGURATION}.xcconfig << 'EOF'
+cat > Betzi/Configs/Environment/Confg-${CONFIGURATION}.xcconfig << 'EOF'
 BASE_URL = ${API_KEY}
 API_KEY ${BASE_URL}
 EOF
